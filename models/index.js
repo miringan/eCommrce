b5443570918d8f0676532ffdb25f06a5facffecb
Michael Iringan <miringan@uw.edu>
2022-04-08 16:58:45 -0700

    Updated index.js to include foreign keys in Product.belongsToMany and Tag.belongsToMany nad changed foreignKey of Category.hasMany to catergory_id.

--- models/index.js
+++ models/index.js
@@ -11,13 +11,14 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
+    foreignKey: 'product_id',
     unique: false
   }
 });
@@ -26,6 +27,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    foreignKey: 'tag_id',
     unique: false
   }
 })
